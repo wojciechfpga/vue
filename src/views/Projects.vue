50132694a4b78ee4f5d1191900f092c17c39c290
Wojciech Lasowski <Wojciech.lasowski.poczta.pl@outlook.com>
2024-01-18 17:56:59 +0100

    auth

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,22 +1,30 @@
 <template>
-  <div>
+  <div class="privacyclass">
     <h1>Zrealizowane projekty</h1>
-    <h2>Back-end tej strony</h2>
-    <p>Kod źródłowy tej strony (back-end)</p>
+    <div class="article-box">
+      <div class="article-header">Back-end tej strony</div>
+      <div class="gradient-line"></div>
+      <p>Kod źródłowy tej strony (back-end)</p>
     <div class="link">
-      <a href="https://dev.azure.com/Wojciechlasowskipocztapl/AzureDotNET/_git/WebAPIForAzureCloud">Back-end tej
-        strony</a>
+      <a href="https://dev.azure.com/Wojciechlasowskipocztapl/AzureDotNET/_git/WebAPIForAzureCloud">Link do back-end-u tej
+        strony (C#/ASP >NET)</a>
 
-    <p>Kod Front-end-u w trakcie refakctoryzacji, wkrótce zostanie opublikowany, napisany w Vue.js</p>
-  </div>
-  <div class="column">
-    <div class="styled-box">
-
-      <embed src="https://storagekontowojtek.blob.core.windows.net/article-images/LasowskiApplication_sa.pdf"
-        type="application/pdf" width="100%">
+      <p>Kod Front-end-u w trakcie refakctoryzacji, wkrótce zostanie opublikowany, napisany w Vue.js</p>
     </div>
+    <div class="column">
+      <div class="styled-box">
+
+        <embed src="https://storagekontowojtek.blob.core.windows.net/article-images/LasowskiApplication_sa.pdf"
+          type="application/pdf" width="100%">
+      </div>
+    </div>
+    </div>
+
+    <h2></h2>
+
+
   </div>
-</div></template>
+</template>
 
 <script>
 export default {
@@ -25,9 +33,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    /* Style for the container box */
+    .article-box {
+      max-width: 800px;
+      margin: 20px auto;
+      padding: 20px;
+      border: 1px solid #ddd;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+
+    /* Style for the header */
+    .article-header {
+      font-size: 1.5em;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+
+    /* Style for the gradient line */
+    .gradient-line {
+      width: 80%;
+      height: 2px;
+      background: linear-gradient(to right, #ff8c00, #ff0080); /* Adjust colors as needed */
+      margin: 10px auto;
+    }
+.privacyclass {
+  text-align: justify;
+  width: 50%;
+  line-height: 200%;
+}
+
 .styled-box {
   width: 800px;
-  height: 1000px;
+  height: 500px;
   background-color: #ffffff;
   border: 2px solid #3498db;
   border-radius: 8px;
